24e810022f0d5f6eaf0a35010f21cdd2359a3523
Luis Fernando Hernandez Fernandez <lhernandez@psl.com.co>
2020-06-28 14:13:04 -0500

    Update comonents files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Formulario from "./components/Formulario";
+import ListadoImagenes from "./components/ListadoImagenes";
 
 function App() {
+  const [busqueda, guardarBusqueda] = useState("");
+  const [imagenes, guardarImagenes] = useState([]);
+  const [paginaactual, guardarPaginaActual] = useState(1);
+  const [totalpaginas, guardarTotalPaginas] = useState(1);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      if (busqueda === "") return;
+
+      const imagenesPorPagina = 30;
+      const key = "17238637-b93cf1d7eec1e90511954f048";
+      const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}&page=${paginaactual}`;
+      const respuesta = await fetch(url);
+      const json = await respuesta.json();
+      guardarImagenes(json.hits);
+      // guardar total de paginas
+      const totalpaginas = Math.ceil(json.totalHits / imagenesPorPagina);
+      guardarTotalPaginas(totalpaginas);
+    };
+    consultarAPI();
+  }, [busqueda, paginaactual]);
+
+  // apagina anterior
+
+  const paginaAnterior = () => {
+    const nuevaPaginaActual = paginaactual - 1;
+    if (nuevaPaginaActual === 0) return;
+    guardarPaginaActual(nuevaPaginaActual);
+  };
+
+  const paginaSiguiente = () =>{
+    const nuevaPaginaActual = paginaactual + 1;
+    if (nuevaPaginaActual > totalpaginas) return;
+    guardarPaginaActual(nuevaPaginaActual);
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="jumbotron">
+        <p className="lead text-center text-uppercase"><strong>Buscador de Imágenes de Jacky</strong></p>
+        <Formulario guardarBusqueda={guardarBusqueda} />
+      </div>
+      <div>
+        <ListadoImagenes imagenes={imagenes} />
+      </div>
+      <div className="row justify-content-center">
+        <ListadoImagenes imagenes={imagenes} />
+        { (paginaactual === 1) ? null :
+        <button
+        type="button"
+        className="bbtn btn-info mr-1"
+        onClick={paginaAnterior}
+      >
+        Anterior &raquo;
+      </button>
+        }
+   {(paginaactual === totalpaginas) ? null :
+        <button
+        type="button"
+        className="bbtn btn-info"
+        onClick={paginaSiguiente}
+      >
+        siguiente &raquo;
+      </button>
+   }
+      </div>
     </div>
   );
 }
---
+++ src/components/Formulario.js
@@ -0,0 +1,47 @@
+import React, {useState} from 'react';
+import Error from "./Error"
+
+const Formulario = ({guardarBusqueda}) => {
+const [termino, guardarTermino] = useState("");
+const [error, guardarError] = useState(false);
+
+const buscarImagenes = e => {
+e.preventDefault();
+//Validar
+if (termino.trim() === "")
+{
+    guardarError(true);
+    return;
+}
+// Enviar el termino de búsqueda hacia el componente principal
+guardarBusqueda(termino);
+
+}
+
+    return (  
+        <form
+        onSubmit={buscarImagenes}
+        >
+            <div className="row">
+                <div className="form-group col-md-8">
+                    <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Ingresa la palabra, ejemplo: Mascotas, Novios."
+                    onChange={e=>guardarTermino(e.target.value)}
+                    />
+                </div>
+                <div className="form-group col-md-4">
+                    <input
+                    type="submit"
+                    className="bt btn-lg btn-danger btn-block"
+                    value="Buscar"
+                    />
+                </div>
+            </div>
+            {error ? <Error mensaje="Agregue algo sopenco "/> : null}
+        </form>
+    );
+}
+ 
+export default Formulario
